69da994607fa94f9b6fe39b772914096a69d8d98
chedao_stuff <chedao_stuff@gmail.com>
2023-10-16 13:13:45 +0800

    init

--- ui/src/pages/LoginPage.vue
+++ ui/src/pages/LoginPage.vue
@@ -1,6 +1,20 @@
 
 <template>
-
+ <div>
+   <div class="login">
+     <div class="imgbox" style="background: url(/images/login.jpg) no-repeat center center;background-size: cover;"></div>
+     <div class="center">
+       <div class="logo"><img src="images/logo.png" alt=""></div>
+       <div class="btns">
+         <div class="btn1"><a href="pages.html">WALLET LOGIN</a></div>
+         <div class="btn2">
+           <a href="#">social account</a>
+           <a href="#">white papper</a>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div>
 </template>
 
 <script setup>
