237190fd0cdb7bc1f497ef25c5104505bdc43ba3
chedao_stuff <chedao_stuff@gmail.com>
2023-10-17 14:11:56 +0800

    init

--- ui/src/pages/AdventureListPage.vue
+++ ui/src/pages/AdventureListPage.vue
@@ -1,58 +1,60 @@
 <template>
-<!--  <ElButton @click="connect_wallet">conn</ElButton>-->
-<!--  <ElButton @click="spawn">spawn</ElButton>-->
-<!--  <ElButton @click="test">test</ElButton>-->
+  <!--  <ElButton @click="connect_wallet">conn</ElButton>-->
+  <!--  <ElButton @click="spawn">spawn</ElButton>-->
+  <!--  <ElButton @click="test">test</ElButton>-->
   <div class="creatMain">
-    <a href="#" class="Skip">Skip</a>
-    <div class="contBase  itemNase " >
+<!--    <a href="#" class="Skip">Skip</a>-->
+    <div class="contBase  itemNase " v-if="step===1"  @click="nextStep">
       <div class="title">A mysterious voice:</div>
       <div class="text">
         Welcome to the land of Talas, brave adventurer!
       </div>
       <div class="next"></div>
     </div>
-    <div class="content2 itemNase">
+    <div class="content2 itemNase" v-if="step===2">
       <input type="text" placeholder="Some text about user name" class="words">
       <div class="warning">*Some text about error tips</div>
-      <button class="btnBase">CONFIRM</button>
+      <button class="btnBase" @click="nextStep">CONFIRM</button>
     </div>
-    <div class="contBase itemNase" >
+    <div class="contBase itemNase" v-if="step===3"  @click="nextStep">
       <div class="title">A mysterious voice:</div>
       <div class="text">
-        Talas is located in the southeastern corner of the world, brimming with mystery and magic. It holds valuable mineral resources, especially the enigmatic 'Nightstone,' which draws adventurers.
+        Talas is located in the southeastern corner of the world, brimming with mystery and magic. It holds valuable
+        mineral resources, especially the enigmatic 'Nightstone,' which draws adventurers.
       </div>
       <div class="next"></div>
     </div>
-    <div class="itemNase step3">
-      <div class="contBase  ">
+    <div class="itemNase step3"  v-if="step===4">
+      <div class="contBase" >
         <div class="title">A mysterious voice:</div>
-        <div class="text">
-          Talas is located in the southeastern corner of the world, brimming with mystery and magic. It holds valuable mineral resources, especially the enigmatic 'Nightstone,' which draws adventurers.
+        <div class="text"  >
+          Talas is located in the southeastern corner of the world, brimming with mystery and magic. It holds valuable
+          mineral resources, especially the enigmatic 'Nightstone,' which draws adventurers.
         </div>
         <div class="next"></div>
       </div>
       <div class="listItems">
         <ul>
-          <li>
+          <li @click="awser(0)">
             <div class="num">A</div>
             <div class="ri">Seek help and collaborate with other adventurers to solve the problem</div>
           </li>
-          <li>
+          <li @click="awser(1)">
             <div class="num">B</div>
             <div class="ri">Quickly assess the situation, formulate a plan, and tackle the challenge alone</div>
           </li>
-          <li>
+          <li @click="awser(2)">
             <div class="num">C</div>
             <div class="ri">Utilize your creativity and wisdom to find unconventional solutions.</div>
           </li>
-          <li>
+          <li @click="awser(3)">
             <div class="num">D</div>
             <div class="ri">Seek out mysterious magic or treasures to address the crisis.</div>
           </li>
         </ul>
       </div>
     </div>
-    <div class="content3 itemNase">
+    <div class="content3 itemNase" v-if="step===5">
       <div class="title">
         <img src="images/title.png" alt="">
       </div>
@@ -124,13 +126,21 @@ export default {
   },
   computed: mapState(['wallet_address', "adventurers"]),
   data() {
-    return {}
+    return {
+      step: 1
+    }
   },
   methods: {
     ...mapActions(['connect_wallet', 'start', 'getReceipt']),
     ...mapMutations(['']),
-    enter(){
+    enter() {
       this.$router.push('/main')
+    },
+    nextStep(){
+      this.step++;
+    },
+    awser(value){
+      this.step++;
     },
     async spawn() {
       await this.start({
