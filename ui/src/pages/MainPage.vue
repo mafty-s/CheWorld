e2e0bdbced26359a72995732f5b03e1cc9c1dbd6
chedao_stuff <chedao_stuff@gmail.com>
2023-10-18 14:10:06 +0800

    1

--- ui/src/pages/MainPage.vue
+++ ui/src/pages/MainPage.vue
@@ -38,19 +38,19 @@
     <div class="controls">
       <ul>
         <li>
-          <img src="images/set1.png" alt="">
+          <img src="@/assets/images/set1.png" alt="">
           <div class="num">99</div>
         </li>
         <li>
-          <img src="images/set2.png" alt="">
+          <img src="@/assets/images/set2.png" alt="">
           <div class="num">99</div>
         </li>
         <li>
-          <img src="images/set3.png" alt="">
+          <img src="@/assets/images/set3.png" alt="">
           <div class="num">99</div>
         </li>
         <li>
-          <img src="images/set4.png" alt="">
+          <img src="@/assets/images/set4.png" alt="">
           <div class="num">99</div>
         </li>
         <li></li>
