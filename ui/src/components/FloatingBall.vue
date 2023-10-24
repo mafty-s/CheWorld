040a3de537df0dc6128477492a7a3d9017802b29
chedao_stuff <chedao_stuff@gmail.com>
2023-10-24 17:20:51 +0800

    1

--- ui/src/components/FloatingBall.vue
+++ ui/src/components/FloatingBall.vue
@@ -14,7 +14,7 @@ export default {
     return {}
   },
   methods: {
-    getMaxHealth(){
+    getMaxHealth() {
       const vitality = this.adventurer.vitality ?? 0;
       const maxHealth = Math.min(100 + vitality * 10, 720);
       return maxHealth;
@@ -26,20 +26,20 @@ export default {
 <template>
   <div>
     <div class="typeItem type0">
-<!--      <div class="add"></div>-->
+      <!--      <div class="add"></div>-->
       <div class="ov">
         <div class="icon"><img src="@/assets/images/icon1.png" alt=""></div>
-        <div class="val">{{ adventurer.health }}/{{getMaxHealth()}}</div>
-        <div class="line"></div>
+        <div class="val">{{ adventurer.health }}/{{ getMaxHealth() }}</div>
+        <div class="line" :style="[{ height: (adventurer.health / getMaxHealth()) * 100 + '%' }]"></div>
       </div>
     </div>
 
     <div class="typeItem type2" style="top:150px">
-<!--      <div class="add"></div>-->
+      <!--      <div class="add"></div>-->
       <div class="ov">
         <div class="icon"><img src="@/assets/images/icon2.png" alt=""></div>
         <div class="val">0/0</div>
-        <div class="line"></div>
+        <div class="line" style="height: 0%"></div>
       </div>
     </div>
 
